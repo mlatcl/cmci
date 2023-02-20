54fedfa8aee8c1927745ace6b18e43314f07e530
Eric Meissner <meissner.eric.7@gmail.com>
2023-02-20 11:32:25 +0000

    Optimization in save_segments (array modifications). Add profiling.

--- src/save_segments.py
+++ src/save_segments.py
@@ -7,8 +7,8 @@ from callfinder import CallFinder
 from audio.audio_processing import get_spectrum
 import argparse
 import json
-
 import os
+import time
 
 
 def download_s3_folder(bucket, s3_folder, local_dir=None):
@@ -44,8 +44,9 @@ def save_segments(files_and_segments, file_name='segments.json', dir='', is_DDB=
 
 if __name__ == '__main__':
 
-    start_time = 0
+    start = time.time()
     callFinder = CallFinder()
+
 
     parser = argparse.ArgumentParser(
                     prog = 'CallSegmenter',
@@ -55,6 +56,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
     input_dir = args.input
     output_dir = args.output
+    print("Time elapsed at {}: {}s".format("Beginning", time.time() - start))
 
 
     is_s3 = False
@@ -69,27 +71,36 @@ if __name__ == '__main__':
 
     walked_files = os.walk(root_dir) # [(dir, [dirs], [files]), (dir2, [dirs2], [files2])]
     files_and_segments = {}
+    print("Time elapsed at {}: {:.2f}s".format("Files walked", time.time() - start))
 
     for (dir_name, directories, files) in walked_files:
         print("Walking directory {}".format(dir_name))
-        for file in files:
+        for i, file in enumerate(files):
+            # print("Time elapsed at start of file {}: {:.2f}s".format(i, time.time() - start))
             if file.endswith(".wav"):
-                print("\tProcessing file {}".format(file))
                 sampling_rate, audio = wav.read(os.path.join(dir_name, file))
+                audio = audio[:, 0].astype('f')/1000 # take first channel, scale values down by 1000.
                 length_secs_audio = (audio.shape[0] // sampling_rate)
                 segment_length = 10 if length_secs_audio > 60 else length_secs_audio - 1 # seconds, width of the spectrum we find_calls over
                 # print("\tAUDIO {} {}".format(audio.shape, length_secs_audio))
-                for curr_time in range(0, length_secs_audio-segment_length, segment_length):
-                    # print("\t\tCurrent time {}".format(curr_time))
-                    t, f, S = get_spectrum(start_time=curr_time, sampling_rate=sampling_rate, audio=audio, segment_length=segment_length)
+
+                segment_times = range(0, length_secs_audio-segment_length, segment_length)
+                print("\tProcessing file {} with {} {}s long segments".format(file, len(segment_times), segment_length))
+
+                for curr_time in segment_times:
+                    # print("Time elapsed start of time in file {}: {:.2f}s".format("curr_time", time.time() - start))
+                    S, f, t = get_spectrum(start_time=curr_time, sampling_rate=sampling_rate, audio=audio, segment_length=segment_length)
+                    # print("Time elapsed after {}: {:.2f}s".format("get_spectrum", time.time() - start))
                     segments, thresholded_spectrum, features, final_feature = callFinder.find_calls(S, f, t)
+                    # print("Time elapsed after {}: {:.2f}s".format("find_calls", time.time() - start))
                     full_name = "s3://" + bucket + os.path.join(dir_name, file) if is_s3 else os.path.join(dir_name, file)
                     if full_name in files_and_segments:
                         files_and_segments[full_name] = {'segments': files_and_segments[full_name]['segments'] + segments.tolist()}
                     else:
                         files_and_segments[full_name] = {'segments': segments.tolist()}
-        print()
+                    # print()
 
     save_segments(files_and_segments)
+    print("Time elapsed at {}: {:.2f}s".format("after saving final", time.time() - start))
     
     pass
