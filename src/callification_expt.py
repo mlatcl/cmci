ef6edf6b792a4e97b9feed7ff640c7ca68a0f5d7
Eric Meissner <meissner.eric.7@gmail.com>
2023-04-20 15:23:37 +0100

    Remove unused files.

--- src/callification_expt.py
+++ src/callification_expt.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import json
 from tqdm import trange
 from functools import lru_cache
 import matplotlib.pyplot as plt; plt.ion()
@@ -20,7 +21,7 @@ from sklearn.preprocessing import LabelEncoder
 
 DATA_LOC = '../data/Calls For ML/'
 
-def process(f, start, end, n_fft_prop=1/3):
+def process(f, start, end, sr, n_fft_prop=1/3):
     a = read_audio(f + '.wav')[int(start * sr):int(end * sr)]
     # S = spectrogram(a, nperseg=len(a)//3, noverlap=len(a)//12, fs=sr)[-1]
     S = np.abs(stft(a,
@@ -44,26 +45,30 @@ def additional_features(spectrogram, start, end):
     ])
     return additional_features
 
-@lru_cache(maxsize=10)
+@lru_cache(maxsize=50)
 def read_audio(f):
     return wav.read(os.path.join(DATA_LOC, f))[1].mean(axis=1)
 
 if __name__ == '__main__':
+    CALLS_FILE='Calls_ML_Fix.xlsx'
+    AUDIO_FILE='ML_Test.wav'
 
-    calls = pd.read_excel(os.path.join(DATA_LOC, 'Calls_ML_Fix.xlsx'))
+
+    calls = pd.read_excel(os.path.join(DATA_LOC, CALLS_FILE))
     calls.columns = [c.lower().replace(' ', '_') for c in calls.columns]
-    calls = calls.loc[~calls.call_type.isna() | (calls.interference == 'Conure')].reset_index(drop=True)
-    calls.loc[calls.call_type.isna(), 'call_type'] = 'interference'
+    calls = calls.loc[~calls.call_type.isna() | (calls.interference == 'Conure')].reset_index(drop=True) #drop conure calls?
+    calls.loc[calls.call_type.isna(), 'call_type'] = 'interference' # set all unknown call types to interference
     calls = calls.loc[calls.start < calls.end].reset_index(drop=True)
     calls['call_type'] = calls.call_type.apply(lambda r: r.split(' ')[0])
 
+    # Reclassify call clusters
     # calls.loc[calls.call_type.isin(['Phee', 'Trill', 'Whistle']), 'call_type'] = 'LongCalls'
     # calls.loc[calls.call_type.isin(['Cheep', 'Chuck', 'Tsit']), 'call_type'] = 'ShortCalls'
 
-    sr, _ = wav.read(os.path.join(DATA_LOC, 'ML_Test.wav'))
+    sr, _ = wav.read(os.path.join(DATA_LOC, AUDIO_FILE))
 
     X = np.vstack([
-        process(*calls.loc[i, ['file', 'start', 'end']])
+        process(*calls.loc[i, ['file', 'start', 'end']], sr=sr)
         for i in calls.index
     ])
     X = (X - X.mean(axis=0)) / (X.std(axis=0) + 1e-12)
@@ -73,7 +78,7 @@ if __name__ == '__main__':
     le.fit(y)
     y_transformed = le.transform(y)
 
-    accs = np.zeros(10)
+    accs = np.zeros(10) #number of initializations
     for seed in trange(len(accs)):
         X_train, X_test, y_train, y_test = train_test_split(X, y_transformed, test_size=0.2, random_state=seed)
 
@@ -111,14 +116,13 @@ if __name__ == '__main__':
     plot_df = pd.DataFrame(dict(latent_a=Z[:, 0], latent_b=Z[:, 1], call_type=y))
     sns.scatterplot(data=plot_df, x='latent_a', y='latent_b', hue='call_type', palette='Paired')
 
-    import json
 
-    sr, _ = wav.read(os.path.join(DATA_LOC, 'ML_Test.wav'))
-    with open('../data/Calls for ML/segments_ML_Test.json', 'rb') as file:
+    # Load segment file generated from CallFinder
+    with open(DATA_LOC + 'segments_ML_Test.json', 'rb') as file:
         segments = json.load(file)
 
     X_unlab = np.vstack([
-        process('ML_Test', start, end)
+        process('ML_Test', start, end, sr=sr)
         for (start, end) in segments['data/ML_Test.wav']['segments']
     ])
     X_unlab = (X_unlab - X_unlab.mean(axis=0)) / (X_unlab.std(axis=0) + 1e-12)
