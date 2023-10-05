02e65dcf7329025e6049ef52da23a913ebad8c68
aditya <infprobscix@gmail.com>
2023-10-05 10:43:01 +0100

    Update classification script

--- src/classification_expt.py
+++ src/classification_expt.py
@@ -1,71 +1,67 @@
 
-import os
+import os, torch, wandb
 import numpy as np
-import pandas as pd
-import seaborn as sns
-import json
 from tqdm import trange
 from functools import lru_cache
 import matplotlib.pyplot as plt; plt.ion()
 
-from umap import UMAP
 from librosa.feature import mfcc
 from librosa.core.spectrum import stft
 from scipy.io import wavfile as wav
-from scipy.signal import spectrogram
 
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from sklearn.preprocessing import LabelEncoder
-from utils import preprocess_call_labels
 
-DATA_LOC = '../data/Calls for ML/Banham_Training/'
+softmax = torch.nn.Softmax(dim=1)
 
-def process_file(f, start, end, sr, n_fft_prop=1/3):
-    a = read_audio(f + '.wav')[int(start * sr):int(end * sr)]
-    # S = spectrogram(a, nperseg=len(a)//3, noverlap=len(a)//12, fs=sr)[-1]
+def process_file(f, start, end, n_fft_prop=1/3):
+    sr, a = read_audio(f + '.wav')
+    a = a[int(start * sr):int(end * sr)]
+
     S = np.abs(stft(a,
         n_fft=int(len(a) * n_fft_prop),
         hop_length=int(len(a) * n_fft_prop/2
     )))
+
     mel_features = mfcc(S=S, n_mfcc=20)
     mel_features = (mel_features - mel_features.mean()) / (mel_features.std() + 1e-6)
-
-    features = np.hstack([
-        mel_features.reshape(-1),
-        additional_features(S, start, end)
-    ])
-    return features
-
-def additional_features(spectrogram, start, end):
-    duration = end - start
-    additional_features = np.hstack([
-        duration,
-    ])
-    return additional_features
+    return mel_features.reshape(-1)
 
 @lru_cache(maxsize=50)
 def read_audio(f):
-    return wav.read(os.path.join(DATA_LOC, f))[1].mean(axis=1)
+    sr, audio = wav.read(os.path.join(DATA_LOC, f))
+    if len(audio.shape) == 2:
+        return sr, audio.mean(axis=1)
+    else:
+        return sr, audio.astype(float)
+
+class Classifier(torch.nn.Module):
+    def __init__(self, input_size, num_classes):
+        super().__init__()
+        self.nnet = torch.nn.Linear(input_size, num_classes)
+
+    def forward(self, x):
+        x = self.nnet(x)
+        return softmax(x)
 
 if __name__ == '__main__':
-    CALLS_FILE='Calls_ML.xlsx'
-    AUDIO_FILE='ML_Test.wav'
 
-    calls = pd.read_excel(os.path.join(DATA_LOC, CALLS_FILE))
-    calls = preprocess_call_labels(calls)
+    from call_finder_rnn_simple import AudioDataset, Files, device
+    DATA_LOC = Files.lb_data_loc
 
-    calls = calls.loc[calls.call_type != 'interference'].reset_index(drop=True)
+    data_loader = AudioDataset(device='cpu')
+    calls = data_loader.labels.copy()
+
+    calls = calls.loc[calls.end > calls.start].reset_index(drop=True)
+    calls.loc[calls.call_type == 'Resonating Note', 'call_type'] = 'Resonate'
 
     # Reclassify call clusters
-    calls.loc[calls.call_type.isin(['Phee', 'Trill', 'Whistle']), 'call_type'] = 'LongCalls'
-    calls.loc[calls.call_type.isin(['Cheep', 'Chuck', 'Tsit']), 'call_type'] = 'ShortCalls'
-
-    sr, _ = wav.read(os.path.join(DATA_LOC, AUDIO_FILE))
+    # calls.loc[calls.call_type.isin(['Phee', 'Trill', 'Whistle']), 'call_type'] = 'LongCalls'
+    # calls.loc[calls.call_type.isin(['Cheep', 'Chuck', 'Tsit']), 'call_type'] = 'ShortCalls'
 
     X = np.vstack([
-        process_file(*calls.loc[i, ['file', 'start', 'end']], sr=sr)
+        process_file(*calls.loc[i, ['file', 'start', 'end']])
         for i in calls.index
     ])
     X = (X - X.mean(axis=0)) / (X.std(axis=0) + 1e-12)
@@ -75,31 +71,44 @@ if __name__ == '__main__':
     le.fit(y)
     y_transformed = le.transform(y)
 
-    accs = np.zeros(10) #number of initializations
-    for seed in trange(len(accs)):
-        X_train, X_test, y_train, y_test = train_test_split(X, y_transformed, test_size=0.2, random_state=seed)
+    X_train, X_test, y_train, y_test = train_test_split(X, y_transformed, test_size=0.2, random_state=42)
 
-        dim_reducer = UMAP(random_state=seed).fit(X_train)
-        Z_train = dim_reducer.transform(X_train)
-        Z_test = dim_reducer.transform(X_test)
+    X_train = torch.tensor(X_train).float().to(device)
+    y_train = torch.tensor(y_train).float().to(device)
+    X_test = torch.tensor(X_test).float().to(device)
 
-        classifier = KNeighborsClassifier().fit(Z_train, y_train)
-        accs[seed] = (classifier.predict(Z_test) == y_test).mean()
-    accs *= 100
-    print(f'Accuracy:{accs.mean().round(2)}%±{2*accs.std().round(1)}%')
-    # 85.5% ± 4.8% grouped
+    classifier = Classifier(X.shape[1], len(le.classes_))
+
+    optimizer = torch.optim.Adam([
+        dict(params=classifier.parameters(), lr=0.01),
+    ])
+
+    wandb.init(project="monke")
+
+    losses = []; iterator = trange(10000, leave=False)
+    for i in iterator:
+        optimizer.zero_grad()
+
+        y_prob = classifier(X_train)
+        loss = -torch.distributions.Categorical(y_prob).log_prob(y_train).sum()
+
+        tr_cm = confusion_matrix(y_train.cpu(), y_prob.argmax(dim=1).detach().cpu(), normalize='all')*100
+        tr_cm = tr_cm[range(len(tr_cm)), range(len(tr_cm))].sum().round(2)
+
+        te_cm = confusion_matrix(y_test, classifier(X_test).argmax(dim=1).detach().cpu(), normalize='all')*100
+        te_cm = te_cm[range(len(te_cm)), range(len(te_cm))].sum().round(2)
+
+        losses.append(loss.item())
+        iterator.set_description(f'L:{np.round(loss.item(), 2)},Tr:{tr_cm}%,Te:{tr_cm}%')
+        wandb.log(dict(class_l=loss.item(), class_tr=tr_cm, class_te=te_cm))
+        loss.backward()
+        optimizer.step()
 
     ConfusionMatrixDisplay(
         confusion_matrix(
             le.inverse_transform(y_test),
-            le.inverse_transform(classifier.predict(Z_test)),
+            le.inverse_transform(classifier(X_test).argmax(dim=1).detach().cpu()),
             normalize='true'
         ).round(2),
-        display_labels=le.classes_
+        display_labels=le.classes_.copy()
     ).plot()
-
-    Z = np.vstack([Z_train, Z_test])
-    plot_y = le.inverse_transform(np.hstack([y_train, y_test]))
-
-    plot_df = pd.DataFrame(dict(latent_a=Z[:, 0], latent_b=Z[:, 1], call_type=plot_y))
-    sns.scatterplot(data=plot_df, x='latent_a', y='latent_b', hue='call_type', palette='Paired')
